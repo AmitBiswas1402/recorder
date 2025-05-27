5b95cba8b8686a39d8fe62fb3f5a82433564436d
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-05-27 19:56:11 +0530

    Refactor Header component; add user image display and upload link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
   return (
@@ -8,7 +9,22 @@ const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
                 {userImg && (
                     <Image src={userImg || '/assets/images/dummy.jpg'} alt="user" className="rounded-full" height={66} width={66} />
                 )}
+
+                <article>
+                  <p>{subHeader}</p>
+                  <h1>{title}</h1>
+                </article>
             </div>
+
+            <aside>
+              <Link href="/upload">
+                <Image src="/assets/icons/upload.svg" alt="upload" width={16} height={16} />
+                <span>Upload a Video</span>
+              </Link>
+              <button>
+                
+              </button>
+            </aside>
         </section>
     </header>
   )
