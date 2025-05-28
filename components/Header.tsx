d00480f6d68ec93cfcede1f0467eca85d6ceb488
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-05-28 18:31:21 +0530

    Refactor Header component; integrate record button and update icon imports

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,56 @@
-import Image from "next/image"
-import Link from "next/link"
+import { ICONS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
 
 const Header = ({ subHeader, title, userImg }: SharedHeaderProps) => {
   return (
     <header className="header">
-        <section className="header-container">
-            <div className="details">
-                {userImg && (
-                    <Image src={userImg || '/assets/images/dummy.jpg'} alt="user" className="rounded-full" height={66} width={66} />
-                )}
+      <section className="header-container">
+        <div className="details">
+          {userImg && (
+            <Image
+              src={userImg || "/assets/images/dummy.jpg"}
+              alt="user"
+              className="rounded-full"
+              height={66}
+              width={66}
+            />
+          )}
 
-                <article>
-                  <p>{subHeader}</p>
-                  <h1>{title}</h1>
-                </article>
-            </div>
+          <article>
+            <p>{subHeader}</p>
+            <h1>{title}</h1>
+          </article>
+        </div>
 
-            <aside>
-              <Link href="/upload">
-                <Image src="/assets/icons/upload.svg" alt="upload" width={16} height={16} />
-                <span>Upload a Video</span>
-              </Link>
-              <button>
-                
-              </button>
-            </aside>
-        </section>
+        <aside>
+          <Link href="/upload">
+            <Image
+              src="/assets/icons/upload.svg"
+              alt="upload"
+              width={16}
+              height={16}
+            />
+            <span>Upload a Video</span>
+          </Link>
+          <div className="record">
+            <button className="primary-btn">
+              <Image src={ICONS.record} alt="record" width={16} height={16} />
+              <span>Record a video</span>
+            </button>
+          </div>
+        </aside>
+      </section>
+
+      <section className="search-filter">
+        <div className="search">
+          <input type="text" 
+            placeholder="Search for videos, tags, folders"
+          />
+          <Image src="/assets/icons/search.svg" alt="search" width={16} height={16} />
+        </div>
+      </section>
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
