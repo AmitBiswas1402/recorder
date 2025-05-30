b79f5e9dd1a09d77f086e03f16c3c68861824113
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-05-30 13:33:48 +0530

    Add SignIn page, VideoCard component, and update Profile and Page components to display video cards; update package dependencies

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,13 +1,17 @@
 import Header from "@/components/Header";
+import VideoCard from "@/components/VideoCard";
+import { dummyCards } from "@/constants";
 
 const Profile = async ({params} : ParamsWithSearch) => {
     const { id } = await params;
   return (
     <div className="wrapper page">
         <Header subHeader="amit@mail.com" title="Amit" userImg="/assets/images/dummy.jpg" />
-        <h1 className="text-2xl font-karla">
-            ID: {id}
-        </h1>
+        <section className="video-grid">
+        {dummyCards.map((card) =>(
+          <VideoCard key={card.id} {...card} />
+        ))}
+      </section>
     </div>
   )
 }
---
+++ app/(root)/video/[videoId]/page.tsx
@@ -0,0 +1,8 @@
+const page = () => {
+  return (
+    <div className="wrapper page">
+        Video Page
+    </div>
+  )
+}
+export default page
